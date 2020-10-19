9728407da52f1e05a1a4832d8a9f7057e50045bb
Gururaj-Gidaveer <Gururaj.Gidaveer@mmc.com>
2020-10-19 20:39:22 +0530

    Example to access the content of visual studio files into GITHUB

---
+++ IntroductionOOPS/Polymorphismandoverride.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionOOPS
+{
+    public class Employee
+    {
+       public String FirstName = "FN";
+        public String LastName = "LN";
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+    public class PartTimeEmployee : Employee
+    {
+        public void printFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "- Part Time");
+        }
+    }
+    public class FullTimeEmployee : Employee
+    {
+        public void printFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "- Full Time");
+        }
+    }
+    public class TemporaryEmployee : Employee
+    {
+        public void printFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "- Temporary");
+        }
+    }
+    public class program
+    {
+        public static void Main()
+         {
+            Employee[] employees = new Employee[4];
+
+             employees[0] = new Employee();
+             employees[1] = new PartTimeEmployee();
+             employees[2] = new FullTimeEmployee();
+             employees[3] = new TemporaryEmployee();
+
+        foreach (Employee e in employees)
+        {
+            e.PrintFullName();
+        }
+     }
+    }
+   
+}
---
+++ IntroductionOOPS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionOOPSInher
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("ParentClass Construtor called");
+        }
+        public ParentClass(String Message)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+    public class ChildClaas : ParentClass
+    {
+        public ChildClaas():base("Derived class controlling parent class")// "base " used to specify which constructor is called when we have more then two Constructor with Same Class Nmae
+        {
+            Console.WriteLine("ChildClass Constructor Called");
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            ChildClaas CC = new ChildClaas();
+        }
+    }
+}
